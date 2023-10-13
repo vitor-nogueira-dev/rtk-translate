66b23699128b8934b196b370b393478e649e4c9b
Vitor Nogueira <vslnogueira96@gmail.com>
2023-10-13 10:38:08 -0300

    chore: creating placeholder translation object

---
+++ src/data/searchTranslate.ts
@@ -0,0 +1,53 @@
+export type SearchTranslate = {
+  [key: string]: {
+    searchPlaceholder: string
+  }
+}
+
+const languagesSearch: SearchTranslate = {
+  pt: { searchPlaceholder: 'Pesquisar...' },
+  en: { searchPlaceholder: 'Search...' },
+  es: { searchPlaceholder: 'Buscar...' },
+  fr: { searchPlaceholder: 'Rechercher...' },
+  de: { searchPlaceholder: 'Suchen...' },
+  it: { searchPlaceholder: 'Cerca...' },
+  ru: { searchPlaceholder: 'Поиск...' },
+  ja: { searchPlaceholder: '検索...' },
+  zh: { searchPlaceholder: '搜索...' },
+  ar: { searchPlaceholder: 'بحث...' },
+  hi: { searchPlaceholder: 'खोजें...' },
+  ko: { searchPlaceholder: '검색...' },
+  nl: { searchPlaceholder: 'Zoeken...' },
+  pl: { searchPlaceholder: 'Szukaj...' },
+  tr: { searchPlaceholder: 'Ara...' },
+  vi: { searchPlaceholder: 'Tìm kiếm...' },
+  id: { searchPlaceholder: 'Cari...' },
+  th: { searchPlaceholder: 'ค้นหา...' },
+  sv: { searchPlaceholder: 'Sök...' },
+  cs: { searchPlaceholder: 'Hledat...' },
+  ro: { searchPlaceholder: 'Căutați...' },
+  hu: { searchPlaceholder: 'Keresés...' },
+  fi: { searchPlaceholder: 'Hae...' },
+  da: { searchPlaceholder: 'Søg...' },
+  sk: { searchPlaceholder: 'Hľadať...' },
+  no: { searchPlaceholder: 'Søk...' },
+  bg: { searchPlaceholder: 'Търсене...' },
+  hr: { searchPlaceholder: 'Pretraži...' },
+  lt: { searchPlaceholder: 'Ieškoti...' },
+  sr: { searchPlaceholder: 'Претрага...' },
+  uk: { searchPlaceholder: 'Пошук...' },
+  el: { searchPlaceholder: 'Αναζήτηση...' },
+  sl: { searchPlaceholder: 'Iskanje...' },
+  lv: { searchPlaceholder: 'Meklēt...' },
+  et: { searchPlaceholder: 'Otsi...' },
+  fa: { searchPlaceholder: 'جستجو...' },
+  he: { searchPlaceholder: 'חיפוש...' },
+  ms: { searchPlaceholder: 'Cari...' },
+  fil: { searchPlaceholder: 'Paghahanap...' },
+  bn: { searchPlaceholder: 'অনুসন্ধান...' },
+  ur: { searchPlaceholder: 'تلاش...' },
+  sw: { searchPlaceholder: 'Tafuta...' },
+  ta: { searchPlaceholder: 'தேடு...' }
+}
+
+export default languagesSearch
